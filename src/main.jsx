147cf0f7c554e492d8808df812da3bd2cc71e2db
auto1easy <auto1easy@naver.com>
2025-03-27 11:47:44 +0900

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import AutoeasyDetailPage from './AutoeasyDetailPage'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AutoeasyDetailPage />
+  </React.StrictMode>,
+)
